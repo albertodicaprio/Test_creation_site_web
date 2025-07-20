13c6bc4caae45910d9ac3e42dfc1d0c24bf9b565
voyageetudesflorence <albert.cirstoiu@gmail.com>
2025-07-20 22:06:22 +0300

    Create getprices.js
    -

---
+++ netlify/functions/getprices.js
@@ -0,0 +1,22 @@
+export async function handler(event, context) {
+  try {
+    const nvdaRes = await fetch(`https://api.twelvedata.com/price?symbol=NVDA&apikey=${process.env.TWELVE_API_KEY}`);
+    const nvdaData = await nvdaRes.json();
+    const usdchfRes = await fetch(`https://api.twelvedata.com/price?symbol=USD/CHF&apikey=${process.env.TWELVE_API_KEY}`);
+    const usdchfData = await usdchfRes.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        currentPrice: nvdaData.price,
+        usdchf: usdchfData.price
+      })
+    };
+  } catch (err) {
+    console.error("Function error:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' })
+    };
+  }
+}
