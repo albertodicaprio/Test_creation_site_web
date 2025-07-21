035616c6d5ebf172cb1ec9d714dc68509d4f2ed2
voyageetudesflorence <albert.cirstoiu@gmail.com>
2025-07-21 22:52:12 +0300

    Update getprices.js
    -

--- netlify/functions/getprices.js
+++ netlify/functions/getprices.js
@@ -2,8 +2,12 @@ export async function handler(event, context) {
   try {
     const nvdaRes = await fetch(`https://api.twelvedata.com/price?symbol=NVDA&apikey=${process.env.TWELVE_API_KEY}`);
     const nvdaData = await nvdaRes.json();
+
     const usdchfRes = await fetch(`https://api.twelvedata.com/price?symbol=USD/CHF&apikey=${process.env.TWELVE_API_KEY}`);
     const usdchfData = await usdchfRes.json();
+
+    console.log("NVDA data:", nvdaData);
+    console.log("USDCHF data:", usdchfData);
 
     return {
       statusCode: 200,
@@ -20,3 +24,4 @@ export async function handler(event, context) {
     };
   }
 }
+
